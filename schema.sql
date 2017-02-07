b17e5b1baa7a0f4b7122beb052e1a5da60bc74c0
lind-08 <lind8584@gmail.com>
2017-02-07 11:31:01 +0300

    Добавлена схема БД

---
+++ schema.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE host (
+    id     INTEGER PRIMARY KEY AUTOINCREMENT
+                   NOT NULL,
+    secret TEXT,
+    name   TEXT
+);
+
+CREATE TABLE usb (
+    id     INTEGER PRIMARY KEY AUTOINCREMENT
+                   NOT NULL,
+    VID    TEXT,
+    PID    TEXT,
+    serial TEXT,
+    name   TEXT
+);
+
+CREATE TABLE rules (
+    id      INTEGER PRIMARY KEY AUTOINCREMENT
+                    NOT NULL,
+    host_id INTEGER REFERENCES host (id) ON DELETE CASCADE
+                                         ON UPDATE CASCADE,
+    usb_id  INTEGER REFERENCES usb (VID) ON DELETE CASCADE
+                                         ON UPDATE CASCADE,
+    value   BOOLEAN DEFAULT FALSE
+);
